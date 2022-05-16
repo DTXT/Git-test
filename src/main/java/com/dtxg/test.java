89afc3609570cbfb1ffdce6091b8c381f2d9f61f
DTXT <2507566674@qq.com>
2022-05-16 14:44:15 +0800

    first commit

---
+++ src/main/java/com/dtxg/test.java
@@ -0,0 +1,13 @@
+package com.dtxg;
+
+/**
+ *@author: lpc
+ *@date: 16/5/2022 下午 2:37
+ *@describe:
+ *@vision
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
